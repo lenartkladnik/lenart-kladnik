39408069b0b6192c8729401b350d714350c9725d
pyth0g <lenart.kladnik@gmail.com>
2026-01-09 12:40:19 +0100

    Minor fixes

--- views.py
+++ views.py
@@ -9,12 +9,13 @@ views = Blueprint('views', __name__)
 all_projects = [
     ('Apr 26, 2025', 'Managing tutoring with a simple frontend.', 'A project for Gimnazija Šentvid', 'https://github.com/lenartkladnik/tutorstvo-website'),
     ('Jan 5, 2025', 'Song recognition program for the terminal.', 'Personal project', 'https://github.com/lenartkladnik/Sngfetch'),
-    ('May 10, 2025', 'Display system information with fully customizable ascii formatting.', 'Personal project', 'https://github.com/lenartkladnik/ascinfo'),
+    ('May 10, 2025', 'Display system information with fully customizable ascii formatting.', 'Personal project', 'https://github.com/lenartkladnik/ascinfo', 'static/media/projects/ascinfo.png'),
     ('Aug 10, 2024', 'A python library for creating cli apps.', 'Personal project', 'https://github.com/lenartkladnik/CliUtils'),
     ('Apr 21, 2024', 'An interpreter for the Brainfuck esolang.', 'Personal project', 'https://github.com/lenartkladnik/Bf-Interpreter'),
     ('Apr 27, 2024', 'A simple 2 player chess game for the terminal.', 'Personal project', 'https://github.com/lenartkladnik/Chess'),
     ('May 25, 2024', 'A simple 1 player uno game for the terminal.', 'Personal project', 'https://github.com/lenartkladnik/Uno'),
-    ('Nov 22, 2024', 'Display ascii weather info in the terminal.', 'Personal project', 'https://github.com/lenartkladnik/Weather')
+    ('Nov 22, 2024', 'Display ascii weather info in the terminal.', 'Personal project', 'https://github.com/lenartkladnik/Weather', 'static/media/projects/weather.png'),
+    ('Jul 21, 2025', 'A music player and downloader in the terminal.', 'Personal project', 'https://github.com/lenartkladnik/musicplayer', 'static/media/projects/musicplayer.png')
 ]
 
 all_projects = sorted(all_projects, key=lambda x: datetime.strptime(x[0], '%b %d, %Y'))[::-1]
